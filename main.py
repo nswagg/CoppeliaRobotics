5cfb782d59faad4eb9e56c208015a621d9c527a6
Nick Waggoner <90349188+nswagg@users.noreply.github.com>
2022-03-16 15:08:24 -0600

    RemoteAPI Setup

--- main.py
+++ main.py
@@ -2,3 +2,17 @@
 @author Nick Waggoner
 
 """
+import sys, time, zmqRemoteApi
+from zmqRemoteApi.clients.python.zmqRemoteApi import RemoteAPIClient
+
+
+client = RemoteAPIClient()
+
+# Get a remote object
+sim = client.getObject('sim')
+
+# Call API Function
+APPFUNC = sim.getObject('/Floor')
+print(APPFUNC)
+
+# clientId = simulation.stopSimulation()
